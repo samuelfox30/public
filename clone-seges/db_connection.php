df9dc1864f710cc369e10bdeef859a9b15e84914
Samuelfox30 <samuelfoxgama@hotmail.com>
2024-10-08 00:27:56 -0300

    hostinger_configurations

--- clone-seges/db_connection.php
+++ clone-seges/db_connection.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = 'localhost';
-$users = 'root';
-$db_password = 'root';
+$host = 'segeshub.com';
+$users = 'u339952661_5u9GF';
+$db_password = 'samM9624!';
 
-$db_name = 'seges';
+$db_name = 'u339952661_X5EYC';
 $conn = new mysqli($host, $users, $db_password, $db_name);
 if ($conn->connect_error){
     echo("Conexão com o db " . $db_name . " falhou: " . $conn->connect_error);
