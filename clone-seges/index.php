c7c6806b24bde9a478999faecb7a8daf4b374740
Samuelfox30 <samuelfoxgama@hotmail.com>
2024-10-07 22:56:09 -0300

    organizated

---
+++ clone-seges/index.php
@@ -0,0 +1,30 @@
+<?php
+
+  
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistema de Educação</title>
+  <link rel="stylesheet" href="src/style/style1.css">
+</head>
+<body>
+  <div class="tudo">
+    <img class="img-principal" src="src/img/logo-p.png" alt="">
+    <div class="form">
+      <form action="" method="post">
+        <input class="cpf-e-senha" name="cpf" type="text" placeholder="Digite seu CPF">
+        <input class="cpf-e-senha" name="senha" type="password" placeholder="Digite sua senha">
+        <img id="mostrar-senha" title="Mostrar senha" src="src/img/olho-senha.png" alt="">
+        <input id="submit" name="submit" type="submit" value="ENTRAR">
+        <a id="esqueci-minha-senha" href="">Esqueci minha senha</a>
+      </form>
+    </div>
+  </div>
+  <img id="appolus" src="src/img/appolus.png" alt="">
+</body>
+</html>
